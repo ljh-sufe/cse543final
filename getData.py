3feba70c5bb7049cc39e29dde11d0b542c2c5f4b
Junhao(Jonathan) Lan <88733865+ljh-sufe@users.noreply.github.com>
2022-04-05 20:02:21 -0500

    1

--- getData.py
+++ getData.py
@@ -12,6 +12,10 @@ sp500cons = pd.read_csv("spxstocks.csv")
 sp500cons = sp500cons.set_index("Date")
 stockList = sp500cons.Ticker.value_counts().index.to_list()
 
+test = True
+if test:
+    stockList = stockList[0:10]
+
 #
 # # get price data (daily) for the computing cov matrix
 # priceData = None
@@ -36,18 +40,38 @@ stockList = sp500cons.Ticker.value_counts().index.to_list()
 #         pass
 
 
+dateList = pd.date_range("2000-01-01", "2022-04-04", freq="BM")
+dateList = [x.__str__()[0:10] for x in dateList]
 
 
-dateList = pd.date_range("2000-01-01", "2022-04-04", freq="M")
+class MAfactor():
+    def __init__(self, factorname, timeperiod):
+        self.name = factorname
+        self.timeperiod = timeperiod
 
 
+# factorsList = ["SMA", "EMA", "WMA", "DEMA", "TEMA", "TRIMA", "KAMA", "MAMA", "HT_TRENDLINE", "MOM"]
+factorsList = []
+factorsList.append(MAfactor("SMA", 20))
+factorsList.append(MAfactor("SMA", 60))
+factorsList.append(MAfactor("SMA", 200))
+factorsList.append(MAfactor("MOM", 20))
+factorsList.append(MAfactor("MOM", 60))
+factorsList.append(MAfactor("MOM", 200))
+factorsList.append(MAfactor("ROC", 20))
+factorsList.append(MAfactor("ROC", 60))
+factorsList.append(MAfactor("ROC", 200))
+factorsList.append(MAfactor("ROCR", 20))
+factorsList.append(MAfactor("ROCR", 60))
+factorsList.append(MAfactor("ROCR", 200))
+factorsList.append(MAfactor("MFI", 20))
+factorsList.append(MAfactor("MFI", 60))
+factorsList.append(MAfactor("MFI", 200))
 
-
-factorsList = ["SMA", "EMA", "WMA", "DEMA", "TEMA", "TRIMA", "KAMA", "MAMA", "HT_TRENDLINE"]
 # get X for predicting return
 for factor in factorsList:
 
-    filepath = "data/" + factor + "/"
+    filepath = "data/" + factor.name + "_" + str(factor.timeperiod) + "/"
     if not os.path.exists(filepath):
         os.makedirs(filepath)
     files = os.listdir(filepath)
@@ -62,63 +86,50 @@ for factor in factorsList:
             nowtime = timeit.default_timer()
             pause = pause + 75
 
-        factorName = makeFactorName(keyword=factor, interval="monthly", time_period=10)
+        factorName = makeFactorName(keyword=factor.name, interval="daily", time_period=factor.timeperiod)
         filename = stock + "_" + factorName + ".csv"
         if filename in files:
             continue
 
-        if "MA" in factor:
-            url = makeurlMA(keyword=factor, stock=stock, interval="monthly", time_period=10)
-        elif "HT" in factor:
-            url = makeurlHT(keyword=factor, stock=stock, interval="monthly")
+        if "MA" in factor.name:
+            url = makeurlMA(keyword=factor.name, stock=stock, interval="daily", time_period=factor.timeperiod)
+        elif "HT" in factor.name:
+            url = makeurlHT(keyword=factor.name, stock=stock, interval="daily")
+        elif factor.name in ["MOM", "ROC", "ROCR", "MFI"]:
+            url = makeurlMA(keyword=factor.name, stock=stock, interval="daily", time_period=factor.timeperiod)
         try:
             r = requests.get(url)
             data = r.json()
             if len(data) == 0:
                 a = pd.Series(index=dateList)
-            elif len(data["Technical Analysis: " + factor]) == 0:
+            elif len(data["Technical Analysis: " + factor.name]) == 0:
                 a = pd.Series(index=dateList)
             else:
-                a = pd.DataFrame(data["Technical Analysis: " + factor]).T[factor]
+                a = pd.DataFrame(data["Technical Analysis: " + factor.name]).T[factor.name]
         except:
             time.sleep(61)
             r = requests.get(url)
             data = r.json()
             if len(data) == 0:
                 a = pd.Series(index=dateList)
-            elif len(data["Technical Analysis: " + factor]) == 0:
+            elif len(data["Technical Analysis: " + factor.name]) == 0:
                 a = pd.Series(index=dateList)
             else:
-                a = pd.DataFrame(data["Technical Analysis: " + factor]).T[factor]
+                a = pd.DataFrame(data["Technical Analysis: " + factor.name]).T[factor.name]
 
         a.name = factorName
-
+        a = a.reindex(dateList)
         a.to_csv(filepath + filename)
     print(factor)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 b = None
 for stock in stockList:
 
     a = None
     for factor in factorsList:
-        filepath = "data/" + factor + "/"
-        d = pd.read_csv(filepath + stock + "_" + factor + "_monthly_10.csv").set_index("Unnamed: 0")
+        filepath = "data/" + factor.name + "_" + str(factor.timeperiod) + "/"
+        d = pd.read_csv(filepath + stock + "_" + factor.name + "_daily_" + str(factor.timeperiod) + ".csv").set_index(
+            "Unnamed: 0")
         if a is None:
             a = d
         else:
@@ -133,6 +144,6 @@ for stock in stockList:
 b.index.name = "time"
 b = b.reset_index().sort_values(by=["time", "stock"]).set_index(["time", "stock"])
 
+c = b.corr()
 
-
-b.corr()
+print(c.values)
